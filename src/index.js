e2cb8c0151629bd45f670ca6fa8dd8bcb6bc251f
Yury G <golovanovya@gmail.com>
2020-05-08 15:54:22 +0300

    tiles moving and composition

--- src/index.js
+++ src/index.js
@@ -14,21 +14,118 @@ const tiles = [];
 const cols = 3;
 const rows = 3;
 const tileSize = 100;
+const tolerance = 20;
+
+const puzzleElement = document.createElement('div');
+puzzleElement.className = 'puzzle';
+const puzzle = {
+    element: puzzleElement,
+    tiles: tiles
+};
+
+let tileGroups = [];
+
 for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
         const tile = {};
+        const group = [];
         const index = (i * cols) + j;
-        tile.y = i;
-        tile.x = j;
+        tile.i = i;
+        tile.j = j;
         tile.index = index;
+        tile.x = tile.j * tileSize + tile.j * 50;
+        tile.y = tile.i * tileSize + tile.i * 50;
         tile.top = hasTop(i) ? tiles[getTop(index, cols)].bottom * -1 : FLAT;
         tile.right = hasRight(j, cols) ? getRandomSide() : FLAT;
         tile.bottom = hasBottom(i, rows) ? getRandomSide() : FLAT;
         tile.left = hasLeft(j) ?  tiles[index - 1].right * -1 : FLAT;
-        tile.element = drawTile(tileSize, tile);
+        tile.element = drawTile(tile);
+        tile.element.addEventListener('mouseover', mouseOverTile);
+        tile.element.addEventListener('tile_drag', (e) => {
+            moveTileWithGroup(tile);
+        });
+        tile.element.addEventListener('tile_finishdrag', (e) => testMatching(tile));
         tiles.push(tile);
+        tile.group = group;
+        group.push(tile);
+        tileGroups.push(group);
     }
 }
+
+function moveTileWithGroup(tile) {
+    const x = parseInt(tile.element.style.left);
+    const y = parseInt(tile.element.style.top);
+    const shiftX = x - tile.x;
+    const shiftY = y - tile.y;
+    for (let i = 0; i < tile.group.length; i++) {
+        const current = tile.group[i];
+        current.x += shiftX;
+        current.y += shiftY;
+        if (current !== tile) {
+            redrawTile(current);
+        }
+    }
+}
+
+function testMatching() {
+    let moved = false;
+    let matched = 0;
+    for (let i = 0; i < tiles.length; i++) {
+        const tile = tiles[i];
+        if (hasLeft(tile.j)) {
+            const left = tiles[tile.index - 1];
+            if (left.group !== tile.group) {
+                const distance = length(tile.x, tile.y, left.x + tileSize, left.y);
+                if (distance > 0 && distance <= tolerance) {
+                    left.x = tile.x - tileSize;
+                    left.y = tile.y;
+                    if (distance > 0) {
+                        redrawTile(left);
+                        moved = true;
+                    }
+                    console.log('left match');
+                    changeGroup(left, tile);
+                    matched++;
+                }
+            }
+        }
+        if (hasTop(tile.i)) {
+            const top = tiles[getTop(tile.index, cols)];
+            const distance = length(tile.x, tile.y, top.x, top.y + tileSize);
+            if (distance <= tolerance) {
+                top.x = tile.x;
+                top.y = tile.y - tileSize;
+                if (distance > 0) {
+                    redrawTile(top);
+                    moved = true;
+                }
+                console.log('top match');
+                changeGroup(top, tile);
+                matched++;
+            }
+        }
+    }
+    console.log(`matched ${matched}`);
+    if (moved) {
+        testMatching();
+    }
+}
+
+function redrawTile(tile) {
+    tile.element.style.left = `${tile.x}px`;
+    tile.element.style.top = `${tile.y}px`;
+}
+
+function changeGroup(from, to) {
+    to.group.push(from);
+    from.group.splice(from.group.indexOf(from), 1);
+    // delete empty group
+    if (from.group.length < 1) {
+        tileGroups.splice(tileGroups.indexOf(from.group), 1);
+    }
+    from.group = to.group;
+}
+
 function hasTop(row) {
     return row !== 0;
 }
@@ -47,12 +144,21 @@ function getTop(index, cols) {
 function getBottom(index, cols) {
     return index + cols;
 }
+/**
+ * Distance between two coords
+ * @param {int} x1 
+ * @param {int} y1 
+ * @param {int} x2 
+ * @param {int} y2 
+ */
+function length(x1, y1, x2, y2) {
+    return Math.abs(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+}
 
-function drawTile(size, tile) {
-    const gap = 50;
+function drawTile(tile) {
     const element = document.createElement('ul');
     element.className = 'puzzle__tile draggable';
-    element.style = `top:${tile.y * size + tile.y * gap}px;left:${tile.x * size + tile.x * gap}px`;
+    element.style = `top:${tile.y}px;left:${tile.x}px`;
     let tileContent = '';
     tileContent += `<li class="puzzle__tile-label puzzle__tile-label_top">${tile.top}</li>`;
     tileContent += `<li class="puzzle__tile-label puzzle__tile-label_left">${tile.left}</li>`;
@@ -60,15 +166,8 @@ function drawTile(size, tile) {
     tileContent += `<li class="puzzle__tile-label puzzle__tile-label_right">${tile.right}</li>`;
     tileContent += `<li class="puzzle__tile-label puzzle__tile-label_bottom">${tile.bottom}</li>`;
     element.innerHTML = tileContent;
-    element.addEventListener('mouseover', mouseOverTile);
     return element;
 }
-
-const puzzleElement = document.createElement('div');
-puzzleElement.className = 'puzzle';
-const puzzle = {
-    element: puzzleElement
-};
 tiles.map(tile => puzzleElement.append(tile.element));
 
 document.querySelector('#puzzle').append(puzzle.element);
@@ -140,6 +239,7 @@ document.addEventListener('mousedown', function (event) {
 
         document.removeEventListener('mousemove', onMouseMove);
         dragElement.removeEventListener('mouseup', onMouseUp);
+        dragElement.dispatchEvent(new Event('tile_finishdrag'));
     }
 
     function moveAt(clientX, clientY) {
@@ -193,7 +293,7 @@ document.addEventListener('mousedown', function (event) {
 
         dragElement.style.left = newX + 'px';
         dragElement.style.top = newY + 'px';
-        dragElement.dispatchEvent(new Event('dragging'));
+        dragElement.dispatchEvent(new Event('tile_drag'));
     }
 
 });
