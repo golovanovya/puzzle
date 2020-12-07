4d4428b7babccd14f22c51f16aca67a0528cfc28
golovanovya <golovanovya@gmail.com>
2020-12-08 02:01:11 +0300

    fix puzzle matching

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import './canvas';
 import {drawCurve} from './canvas.js';
 import Konva from 'konva';
+const {Point, Vector, Tile, Group, Grid} = require('./primitives');
 
 const SIDE_TYPES = {
     VALLEY: -1,
@@ -28,152 +29,6 @@ image.src = 'https://img.fonwall.ru/o/vb/pole-derevo-vozdushnyy-shar-popugay.jpg
 image.onload = () => {
     layer.draw();
 };
-
-class Point {
-    constructor(x, y) {
-        this._x = x;
-        this._y = y;
-    }
-
-    get x() {
-        return this._x;
-    }
-
-    get y() {
-        return this._y;
-    }
-
-    distanceTo(point) {
-        return new Vector(this, point).length();
-    }
-
-    static clone() {
-        new Point(this.x, this.y);
-    }
-
-    x() {
-        return this.x;
-    }
-
-    y() {
-        return this.y;
-    }
-}
-
-class Vector {
-    constructor(start, end) {
-        this.start = new Point(start.x, start.y);
-        this.end = new Point(end.x, end.y);
-    }
-
-    get x() {
-        return this.end.x - this.start.x;
-    }
-
-    get y() {
-        return this.end.y - this.start.y;
-    }
-
-    length() {
-        return Math.abs(Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
-    }
-}
-
-class Element {
-    constructor(...elements) {
-        elements.forEach(element => element.parent = this);
-        this.elements = elements;
-    }
-
-    add(element) {
-        const parent = element.parent;
-        if (parent) {
-            parent.remove(element);
-        }
-        element.parent = this;
-        this.elements.push(element);
-    }
-
-    remove(element) {
-        const index = this.elements.indexOf(element);
-        if (index >= 0) {
-            this.elements.splice(index, 1);
-            element.parent = null;
-        } else {
-            throw new Error("element not found");
-        }
-    }
-}
-
-class Tile extends Element {
-    constructor(position, index, width, height, sides) {
-        super();
-        if (position instanceof Point === false) {
-            throw new Error('position must be a Point class');
-        }
-        if (Array.isArray(sides) === false || sides.length !== 4) {
-            throw new Error('sides must be array with length 4');
-        }
-        this.position = position;
-        this.index = index;
-        this.width = width;
-        this.height = height;
-        this.sides = sides;
-        this.isDragging = false;
-    }
-
-    set moveTo(position) {
-        this.position = position;
-    }
-
-    set move(vector) {
-        this.position = new Point(this.position.x + vector.x, this.position.y + vector.y);
-    }
-
-    get topLeft() {
-        return this.position;
-    }
-    get topRight() {
-        return new Point(this.position.x + this.width, this.position.y);
-    }
-    get bottomLeft() {
-        return new Point(this.position.x, this.position.y + this.height);
-    }
-    get topLeft() {
-        return new Point(this.position.x + this.width, this.position.y + this.height);
-    }
-
-    get top() {
-        return this.sides[0];
-    }
-
-    get right() {
-        return this.sides[1];
-    }
-
-    get bottom() {
-        return this.sides[2];
-    }
-
-    get left() {
-        return this.sides[3];
-    }
-}
-
-class Group extends Element {
-    merge(group) {
-        if (this !== group) {
-            group.elements.forEach(element => super.add(element));
-        }
-    }
-}
-
-class Grid extends Element {
-    constructor() {
-        super();
-        this.tiles = [];
-    }
-}
 
 function getRandomSide() {
     const rand = Math.round(Math.random());
@@ -226,6 +81,8 @@ for (let i = 0; i < ROWS; i++) {
     }
 }
 
+grid.tiles = Object.freeze(grid.tiles);
+
 function moveTileWithGroup(vector, group) {
     group.elements.forEach(item => item.move = vector);
     group.elements.forEach(redrawTile);
@@ -245,58 +102,56 @@ function testMatching(tile) {
         const col = index % COLS;
         if (hasTop(row)) {
             const adjacent = grid.tiles[getTopIndex(index, COLS)];
-            if (checked.some(has(adjacent))) {
-                continue;
-            }
-            const distance = new Vector(adjacent.bottomLeft, current.position);
-            const length = distance.length();
-            if (length <= TOLERANCE) {
-                moveTileWithGroup(distance, adjacent.parent);
-                checking.push(...adjacent.parent.elements);
-                current.parent.merge(adjacent.parent);
+            if (!checked.some(has(adjacent)) && adjacent.parent !== current.parent) {
+                const distance = new Vector(adjacent.bottomLeft, current.position);
+                const length = distance.length();
+                if (length <= TOLERANCE) {
+                    moveTileWithGroup(distance, adjacent.parent);
+                    checking.push(...adjacent.parent.elements);
+                    current.parent.merge(adjacent.parent);
+                }
             }
         }
+        
         if (hasRight(col, COLS)) {
             const adjacent = grid.tiles[index + 1];
-            if (checked.some(has(adjacent))) {
-                continue;
+            if (!checked.some(has(adjacent)) && adjacent.parent !== current.parent) {
+                const distance = new Vector(adjacent.position, current.topRight);
+                const length = distance.length();
+                if (length <= TOLERANCE) {
+                    moveTileWithGroup(distance, adjacent.parent);
+                    checking.push(...adjacent.parent.elements);
+                    current.parent.merge(adjacent.parent);
+                }
             }
-            const distance = new Vector(adjacent.position, current.topRight);
-            const length = distance.length();
-            if (length <= TOLERANCE) {
-                moveTileWithGroup(distance, adjacent.parent);
-                checking.push(...adjacent.parent.elements);
-                current.parent.merge(adjacent.parent);
-            }
-            // console.log(tile);
         }
-        // if (hasLeft(col, COLS)) {
-        //     const adjacent = grid.tiles[index - 1];
-        //     if (checked.some(has(adjacent))) {
-        //         continue;
-        //     }
-        //     const distance = new Vector(adjacent.topRight, current.position);
-        //     const length = distance.length();
-        //     if (length <= TOLERANCE) {
-        //         moveTileWithGroup(distance, adjacent.parent);
-        //         checking.push(...adjacent.parent.elements);
-        //         current.parent.merge(adjacent.parent);
-        //     }
-        //     // console.log(tile);
-        // }
-        // if (hasBottom(row)) {
-        //     const adjacent = grid.tiles[getBottomIndex(index, COLS)];
-        //     if (checked.some(has(adjacent))) {
-        //         continue;
-        //     }
-        //     const distance = new Vector(adjacent.position, current.bottomLeft);
-        //     const length = distance.length();
-        //     if (length <= TOLERANCE) {
-        //         moveTileWithGroup(distance, adjacent.parent);
-        //         checking.push(...adjacent.parent.elements);
-        //         tile.parent.merge(adjacent.parent);
-        //     }
-        // }
+
+        if (hasLeft(col, COLS)) {
+            const adjacent = grid.tiles[index - 1];
+            if (!checked.some(has(adjacent)) && adjacent.parent !== current.parent) {
+                const distance = new Vector(adjacent.topRight, current.position);
+                const length = distance.length();
+                if (length <= TOLERANCE) {
+                    moveTileWithGroup(distance, adjacent.parent);
+                    checking.push(...adjacent.parent.elements);
+                    current.parent.merge(adjacent.parent);
+                }
+            }
+        }
+        
+        if (hasBottom(row, ROWS)) {
+            const adjacent = grid.tiles[getBottomIndex(index, COLS)];
+            if (!checked.some(has(adjacent)) && adjacent.parent !== current.parent) {
+                const distance = new Vector(adjacent.position, current.bottomLeft);
+                const length = distance.length();
+                if (length <= TOLERANCE) {
+                    moveTileWithGroup(distance, adjacent.parent);
+                    checking.push(...adjacent.parent.elements);
+                    current.parent.merge(adjacent.parent);
+                }
+            }
+        }
+
     }
     layer.draw();
     testFinish(tile);
