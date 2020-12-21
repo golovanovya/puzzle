a4199d1a74f61fe07dd645e721a3a36de085592a
golovanovya <golovanovya@gmail.com>
2020-12-21 20:03:39 +0300

    edit layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const config = require('./config');
-
 import './style.css';
 import Konva from 'konva';
 const {Grid} = require('./grid');
@@ -8,13 +6,14 @@ const COLS = 3;
 const ROWS = 3;
 const TILE_SIZE = 100;
 const TOLERANCE = 20;
+const DISPLAY_S = 320;
 
 const WIDTH = (TILE_SIZE + TOLERANCE * 2) * COLS;
 const HEIGHT = (TILE_SIZE + TOLERANCE * 2) * ROWS;
 
 // then create layer
 const stage = new Konva.Stage({
-    container: 'container', // id of container <div>
+    container: 'canvas', // id of container <div>
     width: WIDTH,
     height: HEIGHT
 });
@@ -23,7 +22,7 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 const image = new Image();
-image.src = config.defaultImage;
+image.src = 'https://img.fonwall.ru/o/vb/pole-derevo-vozdushnyy-shar-popugay.jpg?route=mid&amp;h=750';
 image.onload = () => {
     layer.draw();
 };
@@ -33,7 +32,27 @@ const grid = new Grid({
     width: WIDTH,
     height: HEIGHT,
     layer,
-    random: config.mode !== 'development'
+    random: process.env.NODE_ENV === 'production'
 });
 
-window.grid = grid;
+function fitStageIntoParentContainer() {
+    const containerWidth = document.querySelector('.container').offsetWidth;
+
+    // now we need to fit stage into parent
+    let canvasWidth = containerWidth;
+    if (containerWidth < DISPLAY_S) {
+        canvasWidth = DISPLAY_S;
+    }
+    if (containerWidth > WIDTH) {
+        canvasWidth = WIDTH;
+    }
+    // to do this we need to scale the stage
+    const scale = canvasWidth < WIDTH ? canvasWidth / WIDTH : 1;
+
+    stage.width(WIDTH * scale);
+    stage.height(HEIGHT * scale);
+    stage.scale({ x: scale, y: scale });
+    stage.draw();
+}
+fitStageIntoParentContainer();
+window.addEventListener('resize', fitStageIntoParentContainer);
